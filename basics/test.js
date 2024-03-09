14e71aac050f3e8d06aeaea6b998085b30d53ad0
nitinyadav107 <160407157+nitinyadav107@users.noreply.github.com>
2024-03-09 18:33:16 +0000

    save

--- basics/test.js
+++ basics/test.js
@@ -21,10 +21,32 @@
 // // console.log(++y);
 // console.log(y++);
 // console.log("2"===2);
-const id=Symbol("nitin");
-const id1=Symbol("nitin");
-console.log((id===id1));
-const bigNumber=123343213445222222222222222222222n;
-console.log(bigNumber);
-
+// const id=Symbol("nitin");
+// const id1=Symbol("nitin");
+// console.log((id===id1));
+// const bigNumber=123343213445222222222222222222222n;
+// console.log(bigNumber);
+// let name="nitin";
+// let n=name;
+// n="hit";
+// console.log(name);
+// console.log(n);
+// let obj={
+//     name:"nitin",
+//     rollno:12
+// }
+// let obj1=obj;
+// obj1.name="atul"
+// console.log(obj);
+// console.log(obj1);
+let nam="Nitin";
+let Rollno="80";
+console.log(`My Name is ${nam} and my Roll no is ${Rollno}`);
+const gamename= new String("Nitin");
+console.log(gamename[0]);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(2));
+console.log(gamename.substring(0,2));
+console.log(gamename.slice(-2,0));
 
