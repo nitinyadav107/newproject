059ef11c3305a1d76b356f1f1700aa6ffa01d0ad
nitinyadav107 <160407157+nitinyadav107@users.noreply.github.com>
2024-03-10 10:56:50 +0000

    save

---
+++ basics/date_time.js
@@ -0,0 +1,14 @@
+// let date=new Date();
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toLocaleString());
+// let dat=new Date(2023,0,8,6,9);
+// console.log(dat.toLocaleString());
+// let d=Date.now(); //millisec
+// console.log(d);
+// console.log(dat.getTime());
+// console.log(Math.floor(Date.now()/1000)+"sec");  //sec
+let date=new Date();
+console.log(date.getMonth()+1);
+
+
