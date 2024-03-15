e795c5b9ad9cbe479bda629747f4cfd60e80cb28
nitinyadav107 <160407157+nitinyadav107@users.noreply.github.com>
2024-03-15 18:12:26 +0000

    save

--- basics/object2.js
+++ basics/object2.js
@@ -21,9 +21,39 @@
 // console.log(obj.fullname);
 // console.log(obj.fullname.userfullname);
 // console.log(obj.fullname.userfullname.lastname);
-const obj1={1:"a",2:"b"};
-const obj2={3:"a",4:"b"};
+// const obj1={1:"a",2:"b"};
+// const obj2={3:"a",4:"b"};
 // const obj3={obj1 , obj2}
 // const obj3=Object.assign({},obj1,obj2) //if we not write{} then all the object values are stored in obj1
-const obj3={...obj1,...obj2}
-console.log(obj3);
+// const obj3={...obj1,...obj2}
+// console.log(obj3);
+// const arr=[
+//     {
+//         name:"nitin",
+//         rollno:80
+//     },
+//     {
+//         name:"Aditya",
+//         rollno:40
+//     },
+//     {
+//         name:"Aditi",
+//         rollno:20
+//     }
+// ]
+// console.log(arr[1].name);
+// console.log(arr);
+// console.log(Object.keys(arr));
+// console.log(Object.values(arr));
+// console.log(Object.entries(arr));
+// console.log(Object.hasOwnProperty("namee"));
+const course={
+    name:"nitin",
+    rollno:80
+}
+// console.log(course.name);
+// const {name}=course;
+// console.log(name);
+const {name:n}=course;
+console.log(n);
+
