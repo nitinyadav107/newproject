68bd19b25842effc78b61c015d31f1f6274661f2
nitinyadav107 <160407157+nitinyadav107@users.noreply.github.com>
2024-03-22 14:11:20 +0000

    save

--- basics/highorderarrayloop.js
+++ basics/highorderarrayloop.js
@@ -1,4 +1,16 @@
-
-for (const iterator of object) {
+let arr=[1,2,3,4]
+for (const num of arr) {
+    console.log(num);
     
 }
+//map
+// const map=new Map();
+// map.set("In","India");
+// console.log(map);
+const map=new Map();
+map.set("In","India");
+for (const [key,value] of map) {
+    console.log(key+" ======= "+value);
+    
+}
+
