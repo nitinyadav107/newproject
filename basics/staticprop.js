182fe26a828f94ba560fdae54df5c78597e95dbc
nitinyadav107 <160407157+nitinyadav107@users.noreply.github.com>
2024-06-23 10:27:33 +0000

    save

--- basics/staticprop.js
+++ basics/staticprop.js
@@ -5,10 +5,23 @@ class User {
     logMe() {
         console.log(`My Name is ${this.username}`);
     }
+    // static 
     createId() {
         return "123";
     }
 }
-const hitesh = new User("nitin");
-hitesh.logMe();
-console.log(hitesh.createId());
+// const hitesh = new User("nitin");
+// hitesh.logMe();
+// console.log(hitesh.createId()); static is not accessed by object 
+
+class Teacher extends User {
+    constructor(username, email, password) {
+        super(username);
+        super(createId());
+        this.email = email;
+        this.password = password;
+    }
+}
+const obj = new Teacher("nitin", "nitin@gmail.com", "niri");
+console.log(obj);
+// console.log(obj.createId());
